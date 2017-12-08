816538706a2c6ce04c94f2787a040dbb6a85234d
Jack Bryant <468158@mymail.sussexdowns.ac.uk>
2017-12-08 11:18:38 +0000

    Initial stage change.

---
+++ Assets/Score.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public bool resetHighScore;
+
+
+	static int score = 0;
+	static int highScore = 0;
+
+	static Score instance;
+
+	static public void AddPoint() {
+		if(instance.bird.dead)
+			return;
+
+		score++;
+
+		if(score > highScore) {
+			highScore = score;
+		}
+	}
+
+	BirdMovement bird;
+
+	void Start() {
+
+        if (resetHighScore == true)
+        {
+            PlayerPrefs.SetInt("highScore", 0);
+        }
+
+		instance = this;
+		GameObject player_go = GameObject.FindGameObjectWithTag("Player");
+		if(player_go == null) {
+			Debug.LogError("Could not find an object with tag 'Player'.");
+		}
+
+		bird = player_go.GetComponent<BirdMovement>();
+		score = 0;
+		highScore = PlayerPrefs.GetInt("highScore", 0);
+	}
+
+	void OnDestroy() {
+		instance = null;
+		PlayerPrefs.SetInt("highScore", highScore);
+	}
+
+	void Update () {
+		GetComponent<GUIText>().text = "Score: " + score + "\nHigh Score: " + highScore;
+	}
+}
